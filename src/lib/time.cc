ed488a17a973e0720c61c6cdcab51ee4ffbb361b
Christian Plesner Hansen <plesner@ne.utrino.org>
2012-04-04 21:15:22 +1000

    Forgot to remove some code.

--- src/lib/time.cc
+++ src/lib/time.cc
@@ -72,19 +72,6 @@ void Time::sleep(duration_t duration) {
   }
 }
 
-// the prescaler is set so that timer0 ticks every 64 clock cycles, and the
-// the overflow handler is called every 256 ticks.
-#define MICROSECONDS_PER_TIMER0_OVERFLOW (cycles_to_micros(64 * 256))
-
-// the whole number of milliseconds per timer0 overflow
-#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
-
-// the fractional number of milliseconds per timer0 overflow. we shift right
-// by three to fit these numbers into a byte. (for the clock speeds we care
-// about - 8 and 16 MHz - this doesn't lose precision.)
-#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
-#define FRACT_MAX (1000 >> 3)
-
 SIGNAL(TIMER0_OVF_vect) {
   Time::on_overflow();
 }
